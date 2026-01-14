0349f904fb79470a9afb368758b14a39747cd14d
hayato-n8810 <s276185@wakayama-u.ac.jp>
2026-01-14 10:43:51 +0900

    検出箇所切り出しを拡張

--- experiments/ql_analysis/extract_code.py
+++ experiments/ql_analysis/extract_code.py
@@ -4,10 +4,9 @@ from hayalab.config import PathConfig
 
 config = PathConfig()
 
-ql_csv = hayalab.codeql_csv(f"{config.codeql}/output/bachelorQL/id_3.csv")
-
 base_path = str(config.root.parent / "jsPerf" / "no_setup_slow")
-for result in ql_csv:
-    extract_code_block = hayalab.extract_code_block(result, base_path=base_path)
-
-    hayalab.write_file(f"{config.codeql}/output/bachelorQL/id_3/block_{result.path.strip('/')}", extract_code_block)
+for i in range(0, 6):
+    ql_csv = hayalab.codeql_csv(f"{config.codeql}/output/bachelorQL/id_{i + 1}.csv")
+    for result in ql_csv:
+        extract_code_block = hayalab.extract_code_block(result, base_path=base_path)
+        hayalab.write_file(f"{config.codeql}/output/bachelorQL/id_{i + 1}/block_{result.path.strip('/')}", extract_code_block)
